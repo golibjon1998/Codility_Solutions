662506abe7f59effab780cd4ae63edb883b6f296
golibjon1998 <nurmamatovgolibjon@gmail.com>
2022-02-03 18:27:31 +0500

    Initial commit

---
+++ src/BinaryGap.kt
@@ -0,0 +1,30 @@
+fun main() {
+    println(solution(1041))
+}
+
+fun solution(N: Int): Int {
+    val binary = Integer.toBinaryString(N)
+    var t = 0
+    var lastOne = 0
+    for (i in 1 until binary.length) {
+        if (binary[i] == '1') {
+            lastOne = i
+        }
+    }
+
+    var max = 0
+    for (i in 1..lastOne) {
+
+        if (binary[i] != '1') {
+            t++
+        } else {
+            t = 0
+        }
+
+        if (t >= max) {
+            max = t
+        }
+
+    }
+    return max
+}
